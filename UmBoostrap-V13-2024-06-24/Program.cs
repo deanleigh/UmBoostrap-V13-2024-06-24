4b5647751b5ad9bb973ba4e269ff127afdf05489
Dean Leigh <dean.leigh@wholething.co.uk>
2024-06-24 17:33:03 +0100

    Add project files.

---
+++ UmBoostrap-V13-2024-06-24/Program.cs
@@ -0,0 +1,19 @@
+namespace UmBoostrap_V13_2024_06_24
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CreateHostBuilder(args)
+                .Build()
+                .Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
